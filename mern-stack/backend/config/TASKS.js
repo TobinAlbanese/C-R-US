70f32c1c1893b4b7179971dfac19cb9e8eb97b3d
HarkUppal <142873691+HarkUppal@users.noreply.github.com>
2025-04-25 22:50:36 -0700

    SCRUM #9 attempt

---
+++ mern-stack/backend/config/TASKS.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+  service: { type: String, required: true },
+  comments: { type: String },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+taskSchema.index({ date: 1, time: 1, service: 1, user: 1}, { unique: true });
+
+const EmployeeTask = mongoose.model('EmployeeTask', taskSchema);
+export { EmployeeTask };  // export the model
